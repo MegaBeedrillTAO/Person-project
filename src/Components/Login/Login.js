e03380626aa2b3b5082bd6f92227a3cb3f1babb8
MegaBeedrillTAO <jakekremling@yahoo.com>
2019-09-23 11:42:37 -0500

    Got login/register to work on front end.

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,20 +1,62 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {Redirect} from 'react-router-dom';
+import {loginUser, registerUser} from '../../Ducks/Reducers/userReducer.js';
 
 export class Login extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleInput = e => {
+        this.setState({[e.target.name]: e.target.value})
+     }
+
+    handleLogin = () => {
+        const{username, password} = this.state;
+        this.props.loginUser({username, password});
+    }
+
+    handleRegister = () => {
+        const{username, password} = this.state;
+        this.props.registerUser({username, password});
+    }
+    
     render() {
+        if(this.props.user_id){
+           return <Redirect to='/Main'/>
+        }
         return (
             <div className = 'login'>
-                
+                <section>
+                    <p>Username:</p>
+                    <input name='username' onChange={this.handleInput}/>
+                </section>
+                <section>
+                    <p>Password:</p>
+                    <input name='password' onChange={this.handleInput}/>
+                </section>
+                <section>
+                    <button onClick={this.handleLogin}>Login</button>
+                    <button onClick={this.handleRegister}>Register</button>
+                </section>
             </div>
         )
     }
 }
 
-const mapStateToProps = (state) => ({
-    
+const mapStateToProps = (reduxState) => ({
+    user_id: reduxState.userReducer.user_id
 })
 
 
 
-export default connect(mapStateToProps)(Login)
+export default connect(mapStateToProps,{
+    loginUser,
+    registerUser
+})(Login)
--- src/Ducks/Reducers/userReducer.js
+++ src/Ducks/Reducers/userReducer.js
@@ -2,14 +2,59 @@ import Axios from 'axios';
 
 
 const initialState = {
-    user_id: 0,
+    user_id: null,
     username: ''
 }
 
+const REGISTER_USER = 'REGISTER_USER';
+const LOGIN_USER = 'LOGIN_USER';
+const LOGOUT_USER = 'LOGOUT_USER';
+
+
+export function registerUser(newUser) {
+    return {
+       type: REGISTER_USER,
+       payload: Axios.post('/auth/register', newUser)
+    }
+ }
+
+export function loginUser(user) {
+    return {
+       type: LOGIN_USER,
+       payload: Axios.post('/auth/login', user)
+    }
+ }
+
+export function logoutUser() {
+    Axios.post('/auth/logout')
+ 
+    return {
+       type: LOGOUT_USER
+    }
+ }
 
 export default function reducer(state = initialState, action){
     const {type, payload} = action;
     switch(type){
+        case `${REGISTER_USER}_FULFILLED`:
+         return {
+            ...state,
+            user_id: payload.data[0].user_id,
+            username: payload.data[0].username
+         };
+         case `${LOGIN_USER}_FULFILLED`:
+         return {
+            ...state,
+            user_id: payload.data[0].user_id,
+            username: payload.data[0].username
+         };
+         case LOGOUT_USER:
+         return {
+            userId: null,
+            username: '',
+            
+         };
+
         default: return state;
     }
 }
