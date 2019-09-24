73d74abbba5c243921e46e711fcf643d46541a7a
MegaBeedrillTAO <jakekremling@yahoo.com>
2019-09-24 11:34:49 -0500

    Taking a break

--- src/Components/MainView/MainView.js
+++ src/Components/MainView/MainView.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import {getSettings} from '../../Ducks/Reducers/settingsReducer';
 
 
+
 export class MainView extends Component {
     componentDidMount(){
         
